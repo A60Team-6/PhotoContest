02564cc18be0f788ecb538cc54852db3fc2b02f0
MarioBoykov <166829333+MarioBoykov@users.noreply.github.com>
2024-08-23 11:29:39 +0300

    application.properties modified, create_data.sql created, User model created

--- src/main/java/com/telerikacademy/web/photocontest/PhotoContestApplication.java
+++ src/main/java/com/telerikacademy/web/photocontest/PhotoContestApplication.java
@@ -2,13 +2,17 @@ package com.telerikacademy.web.photocontest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+//@EnableJpaRepositories(basePackages = "com.telerikacademy.web.photocontest.config.user")
+//@EntityScan(basePackages = "com.telerikacademy.web.photocontest.config.user")
 @SpringBootApplication
 public class PhotoContestApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(PhotoContestApplication.class, args);
-        System.out.println("Hello");
+
     }
 
 }
---
+++ src/main/java/com/telerikacademy/web/photocontest/models/User.java
@@ -0,0 +1,58 @@
+package com.telerikacademy.web.photocontest.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "users")
+@Data // Генерира автоматично getters, setters, equals, hashCode, toString
+@NoArgsConstructor // Генерира празен конструктор
+@AllArgsConstructor // Генерира конструктор с всички полета
+@Builder // Генерира Builder pattern за създаване на обекти
+public class User {
+
+    @Id
+    @Column(name = "user_id", nullable = false, unique = true)
+    private String userId = UUID.randomUUID().toString();
+
+    @Column(name = "username", nullable = false, unique = true, length = 50)
+    private String username;
+
+    @Column(name = "first_name", nullable = false, length = 50)
+    private String firstName;
+
+    @Column(name = "last_name", nullable = false, length = 50)
+    private String lastName;
+
+    @Column(name = "email", nullable = false, unique = true, length = 50)
+    private String email;
+
+    @Column(name = "password", nullable = false, length = 255)
+    private String password;
+
+    @Column(name = "profile_photo", nullable = false, length = 256)
+    private String profilePhoto;
+
+    @Column(name = "points")
+    private Integer points;
+
+    @ManyToOne
+    @JoinColumn(name = "role_id", nullable = false)
+    private Role role;
+
+    @ManyToOne
+    @JoinColumn(name = "ranking_id", nullable = false)
+    private Ranking ranking;
+
+    @Column(name = "created_at", nullable = false, columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP")
+    private LocalDateTime createdAt = LocalDateTime.now();
+
+    @Column(name = "is_active")
+    private Boolean isActive;
+}
